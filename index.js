7413d8beb141b905cbf1e4347cd3ddec29234099
Alex Kos <alexkos971@gmail.com>
2021-06-22 23:11:16 +0300

    try again

--- index.js
+++ index.js
@@ -43,77 +43,77 @@ const generateCode = () => {
 }
 
 
-// bot.on('message', async (msg) => {
-//     switch(state) {
-//         case 'create':
-//             console.log('create');
-//             let count = parseInt(msg.text);
-//             if (count < 1 || isNaN(count) || count > 20) {
-//                     bot.sendMessage(msg.from.id, "Ошибка! Не верно указанно количество человек");
-//                     return;
-//                 }
-//                 else {
-//                     let code = String(generateCode());
-//                     let disaster = await getDisaster();
+bot.on('message', async (msg) => {
+    switch(state) {
+        case 'create':
+            console.log('create');
+            let count = parseInt(msg.text);
+            if (count < 1 || isNaN(count) || count > 20) {
+                    bot.sendMessage(msg.from.id, "Ошибка! Не верно указанно количество человек");
+                    return;
+                }
+                else {
+                    let code = String(generateCode());
+                    let disaster = await getDisaster();
 
-//                     rooms.push({
-//                         id: code,
-//                         length: count,
-//                         players: [],
-//                         disaster: disaster,
-//                         admin: String(msg.from.id)
-//                     })
-//                     // [code] = {
-//                     //     length: count,
-//                     //     players: [],
-//                     //     admin: msg.from.id
-//                     // };
+                    rooms.push({
+                        id: code,
+                        length: count,
+                        players: [],
+                        disaster: disaster,
+                        admin: String(msg.from.id)
+                    })
+                    // [code] = {
+                    //     length: count,
+                    //     players: [],
+                    //     admin: msg.from.id
+                    // };
                     
-//                     bot.sendMessage(msg.from.id, `Комната на ${count} человек созданна, код - ${code}`);
-//                     bot.sendMessage(msg.from.id, `Чтобы начать игру отправьте /play`);
+                    bot.sendMessage(msg.from.id, `Комната на ${count} человек созданна, код - ${code}`);
+                    bot.sendMessage(msg.from.id, `Чтобы начать игру отправьте /play`);
 
-//                     access.push(String(msg.from.id));
-//                     console.log(access);
-//                     // (async () => {
-//                     //     await getDisaster(code);
-//                     // })();
-//                 }
-//                 state = '';
-//                 break;
+                    access.push(String(msg.from.id));
+                    console.log(access);
+                    // (async () => {
+                    //     await getDisaster(code);
+                    // })();
+                }
+                state = '';
+                break;
                 
-//         case "join":
-//             console.log('join');
-//             let enterCode = await String(msg.text);
+        case "join":
+            console.log('join');
+            let enterCode = await String(msg.text);
             
-//             let check = await rooms.find(e => e.id === enterCode);
+            let check = await rooms.find(e => e.id === enterCode);
             
-//             if (check !== undefined && check.length > 0) {
-//                 if (check.players.length >= check.length) {
-//                     bot.sendMessage(msg.from.id, "Слишком много человек");
-//                     return;
-//                 }
-//                 if (check.players.find(e => e.id === msg.from.id)) {
-//                     bot.sendMessage(msg.from.id, "Вы уже добавлены");
-//                     return;
-//                 }
-//                 else {
-//                     check.players.push({"id": msg.from.id, "card": "", "username": msg.from.username});
-//                     bot.sendMessage(msg.from.id, `Вы добавлены в комнату`);
-//                     bot.sendMessage(check.admin, `Пользователь @${msg.from.username} подключился`)
-//                     state = '';
-//                 }
-//             }
-//             else {
-//                 bot.sendMessage(msg.from.id, "Неверный код");
-//                 return;
-//             }
+            if (check !== undefined && check.length > 0) {
+                if (check.players.length >= check.length) {
+                    bot.sendMessage(msg.from.id, "Слишком много человек");
+                    return;
+                }
+                if (check.players.find(e => e.id === msg.from.id)) {
+                    bot.sendMessage(msg.from.id, "Вы уже добавлены");
+                    return;
+                }
+                else {
+                    check.players.push({"id": msg.from.id, "card": "", "username": msg.from.username});
+                    bot.sendMessage(msg.from.id, `Вы добавлены в комнату`);
+                    bot.sendMessage(check.admin, `Пользователь @${msg.from.username} подключился`)
+                    state = '';
+                }
+            }
+            else {
+                bot.sendMessage(msg.from.id, "Неверный код");
+                return;
+            }
     
-//             state = '';
-//             break;   
+            state = '';
+            break;   
 
-//         default: return;
-//     }
-// })
+        default: return;
+    }
+})
 
 
 bot.on('callback_query', async (query) => {
@@ -297,23 +297,23 @@ bot.onText(/\/(.+)/, async (msg, match) => {
         }  
     });
 
-// bot.onText(/\/end/, (msg) => {
+bot.onText(/\/end/, (msg) => {
     
-//     Object.keys(rooms).forEach(async item => {
-//         if (rooms[item].admin == msg.from.id) {
+    Object.keys(rooms).forEach(async item => {
+        if (rooms[item].admin == msg.from.id) {
             
-//             await delete rooms[item];
-//             bot.sendMessage(msg.from.id, "Комната удалена");   
-//             console.log(rooms);
-//             return;
-//         }
-//         else {
-//             bot.sendMessage(msg.from.id, "Вы не админ");   
-//             return;
-//         }
-//     });
-//     return;
-// });
+            await delete rooms[item];
+            bot.sendMessage(msg.from.id, "Комната удалена");   
+            console.log(rooms);
+            return;
+        }
+        else {
+            bot.sendMessage(msg.from.id, "Вы не админ");   
+            return;
+        }
+    });
+    return;
+});
 
 bot.on('message', (msg, match) => {
     console.log(match);
